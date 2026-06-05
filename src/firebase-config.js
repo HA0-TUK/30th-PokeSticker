68a245cd422c236a639d37bc50d6e76725a62388
HA0-TUK <144315821+HA0-TUK@users.noreply.github.com>
2026-06-05 21:21:37 +0900

    배포 준비

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfhGpFyjNwMjglMlLYr0q1KNA2USIzres",
+  authDomain: "th-poke-trade.firebaseapp.com",
+  projectId: "th-poke-trade",
+  storageBucket: "th-poke-trade.firebasestorage.app",
+  messagingSenderId: "306505836856",
+  appId: "1:306505836856:web:878350779195a4e78879cd",
+  measurementId: "G-G46MXV47G5"
+};
+
+export const firebaseOptions = {
+  enabled: true,
+};
